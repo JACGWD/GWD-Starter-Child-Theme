3c746fabfe54d5e2e4f6dbc2b910dce31e76ab5a
Eric Girouard <gwd@johnabbott.qc.ca>
2021-10-29 07:22:30 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,30 +19,30 @@ add_action( 'wp_enqueue_scripts', 'gwdchildtheme_scripts' );
 
 
 // * INCLUDE GOOGLE FONTS: PRECONNECT  */
-function gwd_starter_theme_googleFontsPreconnect(){
+function gwd_starter_child_theme_googleFontsPreconnect(){
 	echo '<link rel="preconnect" href="https://fonts.gstatic.com" />';
 }
-add_action('wp_head', 'gwd_starter_theme_googleFontsPreconnect', 10);
+add_action('wp_head', 'gwd_starter_child_theme_googleFontsPreconnect', 10);
 
 /* INCLUDE GOOGLE FONTS: THE FONT ITSELF  */
-function gwd_starter_theme_googleFonts(){
+function gwd_starter_child_theme_googleFonts(){
 	wp_enqueue_style( 'google-fonts-lato', 'https://fonts.googleapis.com/css2?family=Lato:wght@900&display=swap"' );
 }
-add_action('wp_enqueue_scripts', 'gwd_starter_theme_googleFonts', 11);
+add_action('wp_enqueue_scripts', 'gwd_starter_child_theme_googleFonts', 11);
 
 
 
 // ADD THE MENU SYSTEM
-  function register_gwdchild_menus() {
-    register_nav_menus(
-      array(
+  // function register_gwdchild_menus() {
+  //   register_nav_menus(
+  //     array(
         // 'header-menu' => __( 'Header Menu' ),  
         // ADD A MENU USING THE ABOVE AS AN EXAMPLE. 
         // DO NOT USE "header-menu", "footer-menu" AND "social-menu" AS THEY ALREADY EXIST IN THE PARENT THEME.
-      )
-    );
-  }
-add_action( 'init', 'register_gwdchild_menus' ); 
+//       )
+//     );
+//   }
+// add_action( 'init', 'register_gwdchild_menus' ); 
 
 
 
